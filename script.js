c1f9d5e410b4d789f97c115dfcab5d988de288b2
Vedprakash321 <131158289+Vedprakash321@users.noreply.github.com>
2023-09-25 00:07:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const numberOfBooksRead = () => {
   // write your code here
 	const n = 0;
 	for (let i = 0; i < library.length; i++) {
-		if(library.readingStatus==true){
+		if(library[i].readingStatus==true){
 			 n++;
 		}
 	}
